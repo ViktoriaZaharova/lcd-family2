d516423b62bb7f413643b9658a13e547ab2b9a2c
Viktoria <zaharova181114@gmail.com>
2022-11-04 18:08:14 +0200

    media

--- app/js/common.js
+++ app/js/common.js
@@ -1,8 +1,8 @@
 $('[name="phone"]').mask('+7 (999) 999-99-99');
 
 $('.about-slider').slick({
-    slidesToShow: 3,
-    vertical: true,
+    slidesToShow: 1,
+
     dots: true,
     arrows: false,
     infinite: false,
@@ -25,17 +25,125 @@ $('.planes-slider').slick({
     nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
 });
 
+$('.location-family-slider1').slick({
+    slidesToShow: 1,
+    variableWidth: true,
+    arrows: false,
+    autoplay: true,
+});
+
+$('.location-family-slider2').slick({
+    slidesToShow: 1,
+    variableWidth: true,
+    arrows: false,
+    autoplay: true,
+});
+
+$('.location-family-slider3').slick({
+    slidesToShow: 1,
+    variableWidth: true,
+    arrows: false,
+    autoplay: true,
+});
+
+$('.location-family-slider4').slick({
+    slidesToShow: 1,
+    variableWidth: true,
+    arrows: false,
+    autoplay: true,
+});
+
+//progressbar
+function setProgress1(index) {
+    const calc = ((index + 1) / ($slider.slick('getSlick').slideCount)) * 100;
+
+    $progressBar
+        .css('width', calc + '%')
+        .attr('aria-valuenow', calc);
+
+}
+
+const $slider = $('.location-family-slider1');
+const $progressBar = $('.progress-bg1');
+
+$slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    setProgress1(nextSlide);
+});
+
+setProgress1(0);
+
+function setProgress2(index) {
+    const calc2 = ((index + 1) / ($slider2.slick('getSlick').slideCount)) * 100;
+
+    $progressBar2
+        .css('width', calc2 + '%')
+        .attr('aria-valuenow', calc2);
+
+}
+
+const $slider2 = $('.location-family-slider2');
+const $progressBar2 = $('.progress-bg2');
+
+$slider2.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    setProgress2(nextSlide);
+});
+
+setProgress2(0);
+
+
+function setProgress3(index) {
+    const calc3 = ((index + 1) / ($slider3.slick('getSlick').slideCount)) * 100;
+
+    $progressBar3
+        .css('width', calc3 + '%')
+        .attr('aria-valuenow', calc3);
+
+}
+
+const $slider3 = $('.location-family-slider3');
+const $progressBar3 = $('.progress-bg3');
+
+$slider3.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    setProgress3(nextSlide);
+});
+
+setProgress3(0);
+
+function setProgress4(index) {
+    const calc4 = ((index + 1) / ($slider4.slick('getSlick').slideCount)) * 100;
+
+    $progressBar4
+        .css('width', calc4 + '%')
+        .attr('aria-valuenow', calc4);
+
+}
+
+const $slider4 = $('.location-family-slider4');
+const $progressBar4 = $('.progress-bg4');
+
+$slider4.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+    setProgress4(nextSlide);
+});
+
+setProgress4(0);
+
+
+$('button[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+    $('.slick-slider').slick('setPosition');
+});
+
+
 $(".input-range").slider({
     range: "min",
     min: 2000000,
     max: 10000000,
     value: 4700000,
-    create: function( event, ui ) {
+    create: function (event, ui) {
         $('.input-range-wrapper').find('.ui-slider-handle').append("<span class='value'></span>");
         $('.input-range-wrapper').find('.value').html('10000');
 
     },
-    slide: function( event, ui ) {
+    slide: function (event, ui) {
         // $('.slider-wrapper2').find
         $('.input-range-wrapper').find('.value').html(ui.value);
     }
