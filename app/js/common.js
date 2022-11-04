4360b4805482b9fe5bcb57bbf71f4db727dd3bac
Viktoria <zaharova181114@gmail.com>
2022-11-04 08:36:50 +0200

    add section

--- app/js/common.js
+++ app/js/common.js
@@ -5,9 +5,24 @@ $('.about-slider').slick({
     vertical: true,
     dots: true,
     arrows: false,
-    infinite: false
-    // variableWidth: true,
+    infinite: false,
     // adaptiveHeight: true
+});
+
+$('.life-family-slider').slick({
+    slidesToShow: 1,
+    variableWidth: true,
+    arrows: false,
+    autoplay: true,
+});
+
+$('.planes-slider').slick({
+    slidesToShow: 2,
+    dots: true,
+    appendArrows: '.planes-slider-nav',
+    appendDots: '.planes-slider-nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
 });
 
 $(".input-range").slider({
